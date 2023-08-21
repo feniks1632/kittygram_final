3473cbf9505784fba56480972501a07ac262de26
feniks1632 <feniks1632@gmail.com>
2023-08-22 04:33:04 +0500

    first push

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,11 +4,13 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+load_dotenv() 
 
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY', '657306')
 
-ALLOWED_HOSTS = []
+DEBUG = os.getenv('DEBUG', 'False') == 'True' 
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',') 
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -86,7 +88,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = os.getenv('TIME_ZONE')
 
 USE_I18N = True
 
