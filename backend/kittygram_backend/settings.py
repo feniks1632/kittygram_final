b7bed555b58a04b6f69a0aab51694c645f6051a3
feniks1632 <feniks1632@gmail.com>
2023-08-22 05:44:46 +0500

    second

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '657306')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['51.250.27.61', '127.0.0.1', 'localhost', 'kittygrams1632.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
